de52c1083e9d6ba1afed186bbf3762c9ba36d99a
Sasha Afanasyev <su@randomlysa.com>
2019-02-25 13:36:01 -0600

    create Home - only has Upload for now

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Upload from "./Upload";
+
+const Home = () => {
+  // Have - Logout - Settings - Upload
+  return (
+    <div>
+      Home
+      <br />
+      <Upload />
+    </div>
+  );
+};
+
+export default Home;
