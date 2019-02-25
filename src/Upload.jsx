57d8c755e2a31c952c85ea83471c1b114e9d052e
Sasha Afanasyev <su@randomlysa.com>
2019-02-25 13:46:46 -0600

    style the upload div, accept only mp3

--- src/Upload.jsx
+++ src/Upload.jsx
@@ -1,42 +1,64 @@
 import React from "react";
-import classNames from "classnames";
 import Dropzone from "react-dropzone";
+import styled from "@emotion/styled";
+
+const getColor = props => {
+  if (props.isDragReject) {
+    return "#c66";
+  }
+  if (props.isDragActive) {
+    return "#6c6";
+  }
+  return "#666";
+};
+
+const Container = styled.div`
+  box-sizing: border-box;
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 300px;
+  border-width: 2px;
+  border-radius: 5px;
+  border-color: ${props => getColor(props)};
+  border-style: ${props =>
+    props.isDragReject || props.isDragActive ? "solid" : "dashed"};
+  background-color: ${props => (props.isDragReject ? "#de4545" : "#eee")};
+`;
 
 class Upload extends React.Component {
   onDrop = (acceptedFiles, rejectedFiles) => {
-    console.log("beep");
     console.log(acceptedFiles, rejectedFiles);
   };
 
-  componentDidMount() {
-    console.log("beep2");
-  }
-
   render() {
     return (
-      <Dropzone onDrop={this.onDrop}>
-        {({ getRootProps, getInputProps, isDragActive }) => {
-          return (
-            <div
-              {...getRootProps()}
-              className={classNames("dropzone", {
-                "dropzone--isActive": isDragActive
-              })}
-            >
-              <input {...getInputProps()} />
-              {isDragActive ? (
-                <p>Drop files here...</p>
-              ) : (
-                <p>
-                  Try dropping some files here
-                  <br />
-                  <br />, or click to select files to upload.
-                </p>
-              )}
-            </div>
-          );
-        }}
-      </Dropzone>
+      <>
+        <Dropzone accept="audio/mp3" onDrop={this.onDrop}>
+          {({
+            getRootProps,
+            getInputProps,
+            isDragActive,
+            isDragAccept,
+            isDragReject,
+            acceptedFiles
+          }) => {
+            return (
+              <Container
+                isDragActive={isDragActive}
+                isDragReject={isDragReject}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                {isDragAccept ? "Drop" : "Drag"} files here...
+              </Container>
+            );
+          }}
+        </Dropzone>
+      </>
     );
   }
 }
