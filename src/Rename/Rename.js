b93effeb95b45a7094ec3a8138bedf75b5c5a477
Sasha Afanasyev <su@randomlysa.com>
2019-02-25 23:47:39 -0600

    set time, date based on current time, date

--- src/Rename/Rename.js
+++ src/Rename/Rename.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import moment from "moment";
 
 const electron = window.require("electron");
 const ipcRenderer = electron.ipcRenderer;
@@ -7,13 +8,24 @@ const Rename = props => {
   const [title, setTitle] = useState("");
   const [speaker, setSpeaker] = useState("");
   const [time, setTime] = useState("");
-  const [day, setDay] = useState("");
+  const [date, setDate] = useState("");
+
+  // Todo - get time from filename.
+  // Set defaults for time, date
+  const timeHM = moment().format("Hm");
+  let morningEvening = " Morning";
+  if (timeHM > 1315) morningEvening = " Evening";
+
+  // Time = "Sunday Morning", "Monday Evening"
+  // Date = MM-DD-YYYY
+  const defaultTime = moment().format("dddd") + morningEvening;
+  const defaultDate = moment().format("MM-DD-YYYY");
 
   const handleChange = e => {
     if (e.target.id === "title") setTitle(e.target.value);
     if (e.target.id === "speaker") setSpeaker(e.target.value);
     if (e.target.id === "time") setTime(e.target.value);
-    if (e.target.id === "day") setDay(e.target.value);
+    if (e.target.id === "day") setDate(e.target.value);
   };
 
   const sendRename = newFileName => {
@@ -23,7 +35,7 @@ const Rename = props => {
   const handleFormSubmit = e => {
     // Probably best to call upload, then call rename.
     e.preventDefault();
-    const newFileName = `${title} - ${speaker} - ${time} - ${day}`;
+    const newFileName = `${title} - ${speaker} - ${time} - ${date}`;
 
     if (props.uploadSucceeded === true) {
       console.log("can rename");
@@ -68,10 +80,17 @@ const Rename = props => {
           type="text"
           onChange={handleChange}
           placeholder="Time"
+          value={defaultTime}
           id="time"
         />
         <br />
-        <input type="text" onChange={handleChange} placeholder="Day" id="day" />
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Date"
+          value={defaultDate}
+          id="date"
+        />
         <br />
         <input type="submit" value="Rename" />
       </form>
