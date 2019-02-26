79d8caa373ca8d785d08c63d39543a9efa15d0cb
Sasha Afanasyev <su@randomlysa.com>
2019-02-25 21:51:06 -0600

    add a Rename component

---
+++ src/Rename/Rename.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const electron = window.require("electron");
+const ipcRenderer = electron.ipcRenderer;
+
+const Rename = () => {
+  return <div>Rename Things ~</div>;
+};
+
+export default Rename;
