0ce7816b16c0c218cdef8b848df30a4e8902c828
Sasha A <su@randomlysa.com>
2019-03-14 22:18:26 -0500

    test loading without token (Login)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, getByText } from "react-testing-library";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+
+it("Shows a welcome and login button when loaded without a token", () => {
+  const { getByText } = render(<App />);
+  getByText("Welcome, please login!");
+  getByText("Login");
 });
